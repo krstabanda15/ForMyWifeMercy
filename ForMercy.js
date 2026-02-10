9a06c044545e8f3000c1a6e1ed5b32d14622d26c
krstabanda15 <k.tabanda29@gmail.com>
2026-02-10 19:17:16 +0800

    Update ForMercy.js

--- ForMercy.js
+++ ForMercy.js
@@ -1,17 +1,5 @@
-const envelope = document.getElementById('envelope');
-const replayBtn = document.getElementById('replay-btn');
-const audio = document.getElementById('valentines-audio');
+const envelope = document.querySelector('.envelope-wrapper');
 
 envelope.addEventListener('click', () => {
-    if (!envelope.classList.contains('is-open')) {
-        envelope.classList.add('is-open');
-        audio.play().catch(e => console.log("Audio requires interaction"));
-    }
-});
-
-replayBtn.addEventListener('click', (e) => {
-    e.stopPropagation();
-    envelope.classList.remove('is-open');
-    audio.pause();
-    audio.currentTime = 0;
+    envelope.classList.toggle('is-open');
 });
