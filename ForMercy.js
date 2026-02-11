569a7afe9300dda2f6d397b6d7b1fddd42c0e2fd
krstabanda15 <k.tabanda29@gmail.com>
2026-02-12 00:56:29 +0800

    Update ForMercy.js

--- ForMercy.js
+++ ForMercy.js
@@ -3,3 +3,28 @@ const envelope = document.querySelector('.envelope-wrapper');
 envelope.addEventListener('click', () => {
     envelope.classList.toggle('is-open');
 });
+
+document.addEventListener("mousemove", (e) => {
+  if (!envelope.classList.contains("is-open")) return;
+
+  const heart = document.createElement("div");
+  heart.className = "trail-heart";
+  heart.innerHTML = "❤️";
+  heart.style.left = e.clientX + "px";
+  heart.style.top = e.clientY + "px";
+
+  document.getElementById("heart-trail").appendChild(heart);
+
+  setTimeout(() => heart.remove(), 1500);
+});
+
+document.addEventListener("mousemove", (e) => {
+  if (!envelope.classList.contains("is-open")) return;
+
+  document.querySelectorAll(".butterfly").forEach((b, i) => {
+    let x = (e.clientX / window.innerWidth) * 200 - 100;
+    let y = (e.clientY / window.innerHeight) * -150;
+
+    b.style.transform = `translate(${x}px, ${y}px)`;
+  });
+});
